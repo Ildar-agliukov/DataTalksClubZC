29eed75c7dc4ed834747d1caddd5ff5b3ff185ff
ildariwe@gmail.com <ildariwe@gmail.com>
2023-02-24 16:15:13 +0000

    final_tables

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -1,30 +1,30 @@
-{{ config(materialized='view') }}
+{{ config(materialized='view')}}
 
--- with tripdata as 
--- (
---   select *,
---     row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
---   from {{ source('staging','fhv_tripdata_clustered') }}
---   where dispatching_base_num is not null 
--- )
+with fhv_data as(
+    select *,
+        'Fhv' as service_type
+    from {{ ref('stf_fhv_tripdata') }}
+)
+
+dim_zones as (
+    select * from {{ ref('dim_zones') }}
+    where borough != 'Unknown'
+)
+
 select
-    -- identifiers
-    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
-    cast(dispatching_base_num as string) as vendorid,
-    cast(pulocationid as integer) as  pickup_locationid,
-    cast(dolocationid as integer) as dropoff_locationid,
-
-    -- timestamps
-    cast(pickup_datetime as timestamp) as pickup_datetime,
-    cast(dropoff_datetime as timestamp) as dropoff_datetime,
-
-    -- trip info
-    sr_flag,
-from {{ source('staging','fhv_tripdata') }}
-where dispatching_base_num is not null 
-
-{% if var('is_test_run', default=true) %}
-
-    limit 100
-
-{% endif %}
+    fhv_data.dispatching_base_num,
+    fhv_dat.service_type,
+    fhv_data.pickup_locationid,
+    pickup_zone.borough as pickup_borough,
+    pickup_zone.zone as pickup_zone,
+    fhv_data.dropoff_locationid,
+    dropoff_zone.borough as dropoff_borough,
+    dropoff_zone.zone as dropoff_zone,
+    fhv_data.pickup_datetime,
+    fhv_data.dropoff_datetime,
+    fhv_data.sr_flag
+from fhv_data
+inner join dim_zones as pickup_zone
+on fhv_data.pickup_locationid = pickup_zone.locationid
+inner join dim_zones as dropoff_zone
+on fhv_data.dropoff_locationid = dropoff_zone.locationid
